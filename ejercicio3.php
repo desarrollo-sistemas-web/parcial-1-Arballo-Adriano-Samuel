3438998d84de2e7151ee28da598a164500db6c27
Arballo-Adriano-Samuel <arballo.samuel2@gmail.com>
2026-05-28 20:37:48 -0300

    Add files via upload

--- ejercicio3.php
+++ ejercicio3.php
@@ -3,6 +3,10 @@
 //Ejercicio 3: Crea una función que tome un número entero como parámetro y devuelva true si el número es par y false si es impar.
 //Complejidad: MEDIA (2 puntos)
 
-function esPar($num) {
-    // Tu código aquí
+function esPar(int $num):int {
+    if ($num % 2 ) {
+        return "int.  True";
+    } else {
+        return "int . false";
+    }
 }
